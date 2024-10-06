95d6ff640d5b1799b5a3193a13124a90a8be5a08
Rark7040 <rarkhopper@gmail.com>
2024-10-06 12:15:06 +0000

    remove invalid submodule

---
+++ app/android/app/src/main/java/com/example/android/app/example/MainActivity.java
@@ -0,0 +1,5 @@
+package com.example.android.app.example;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.android.app.example',
+  appName: 'CapacitorExampleApp',
+  webDir: 'build'
+};
+
+export default config;
